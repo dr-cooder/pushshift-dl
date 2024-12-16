645eab33b16330834a3d3acfa3f769c0f481a56a
Felix Warner <fwarner2319@gmail.com>
2024-12-15 22:45:53 -0500

    Refactored with Estimator object, added autosave

--- pushshift-dl.py
+++ pushshift-dl.py
@@ -30,7 +30,10 @@ except ImportError:
 REDARCS_SUBMISSIONS_FILE_ENDING = '_submissions.zst'
 REDARCS_SUBMISSIONS_FILE_ENDING_LENGTH = len(REDARCS_SUBMISSIONS_FILE_ENDING)
 
+# TODO: Make specifiable
+AUTOSAVE_FREQUENCY = 1000
 IMGUR_CLIENT_ID = '546c25a59c58ad7'
+STREAM_CHUNK_SIZE = 8192
 
 IS_NOT_A_BOOKMARK = 0
 IS_A_BOOKMARK = 1
@@ -123,14 +126,13 @@ class FileManager(object):
         self.__bookmarks = bookmarks
         self.__bookmarks_changed = False
         self.__download_basedirname = download_basedirname
+        self.__downloads_since_last_autosave = 0
         self.__last_bookmark = None if started_without_bookmarks else max(bookmarks)
         self.__prepared_year_folders = set()
         self.__started_done = started_with_end_of_archive_reached and started_without_bookmarks
         self.__started_with_end_of_archive_reached = started_with_end_of_archive_reached
         self.__started_without_bookmarks = started_without_bookmarks
-        self.__user_agent_rotator = UserAgent()
-        self.__change_user_agent()
-        print('User agent is "{}"'.format(self.__user_agent))
+        self.__user_agent_not_initialized = True
 
     @property
     def initial_status(self):
@@ -148,6 +150,12 @@ class FileManager(object):
     def started_without_bookmarks(self):
         return self.__started_without_bookmarks
     
+    def __initialize_user_agent(self):
+        self.__user_agent_rotator = UserAgent()
+        self.__change_user_agent()
+        self.__user_agent_not_initialized = False
+        print('User agent is "{}"'.format(self.__user_agent))
+    
     def __change_user_agent(self):
         self.__user_agent = self.__user_agent_rotator.get_random_user_agent()
 
@@ -159,6 +167,7 @@ class FileManager(object):
         if value not in self.__bookmarks:
             self.__bookmarks_changed = True
         self.__bookmarks.add(value)
+        self.__increment_downloads_since_last_autosave()
     
     def __remove_bookmark(self, value):
         if value in self.__bookmarks:
@@ -179,6 +188,12 @@ class FileManager(object):
         else:
             print('Bookmark unchanged')
         print(time.ctime())
+    
+    def __increment_downloads_since_last_autosave(self):
+        self.__downloads_since_last_autosave += 1
+        if self.__downloads_since_last_autosave >= AUTOSAVE_FREQUENCY:
+            self.write_bookmark(False)
+            self.__downloads_since_last_autosave %= AUTOSAVE_FREQUENCY
 
     def __handle_download(self, year, line_number, download_function, *args, **kwargs):
         if year not in self.__prepared_year_folders:
@@ -191,10 +206,14 @@ class FileManager(object):
                         os.makedirs(day_dir)
             self.__prepared_year_folders.add(year)
         download_function(*args, **kwargs)
+        # If no error occurred while trying to download, then...
+        self.__increment_downloads_since_last_autosave()
         self.__remove_bookmark(line_number)
     
-    def __try_to_get(self, src, is_imgur=False):
+    def __try_to_get(self, src, is_imgur=False, stream=False):
         src_secure = re.sub(r'^(https?:\/\/)?', 'https://', src)
+        if self.__user_agent_not_initialized and is_imgur:
+            self.__initialize_user_agent()
         while True:
             response = requests.get(src_secure, headers = { 'User-Agent': self.__user_agent } if is_imgur else None)
             status_code = response.status_code
@@ -202,13 +221,16 @@ class FileManager(object):
                 self.__change_user_agent()
                 print('Got {} error; Switched user agent to "{}"; Retrying download'.format(status_code, self.__user_agent))
             elif response.url == 'https://i.imgur.com/removed.png':
-                print('Imgur image not found')
+                print('Imgur media not found')
                 return None
             elif response.url == 'https://imgur.com/':
                 print('Imgur page not found')
                 return None
             elif status_code == 200:
-                return response.content
+                if stream:
+                    return response.iter_content(chunk_size=STREAM_CHUNK_SIZE)
+                else:
+                    return response.content
             else:
                 error_message = '{} error'.format(status_code)
                 if status_code == 404:
@@ -217,28 +239,29 @@ class FileManager(object):
                 else:
                     raise Exception(error_message)
     
-    def try_to_get(self, line_number, src, is_imgur=False):
+    def try_to_get(self, line_number, src, is_imgur=False, stream=False):
         content = self.__try_to_get(src, is_imgur=is_imgur)
         self.__remove_bookmark(line_number)
         return content
 
-    def __simple_download(self, download_dirname, download_fileroot, download_main_ext, download_main_src, is_imgur=False):
+    def __simple_download(self, download_dirname, download_fileroot, download_ext, download_src, is_imgur=False):
         content = None
-        download_main_filename = '{}.{}'.format(download_fileroot, download_main_ext)
-        download_main_abspath = os.path.join(download_dirname, download_main_filename)
-        if not os.path.isfile(download_main_abspath):
-            print('Downloading {} from {}'.format(download_main_filename, download_main_src))
-            content = self.__try_to_get(download_main_src, is_imgur=is_imgur)
-            if content:
-                with open(download_main_abspath, 'wb') as download_main:
-                    download_main.write(content)
+        download_filename = '{}.{}'.format(download_fileroot, download_ext)
+        download_abspath = os.path.join(download_dirname, download_filename)
+        if not os.path.isfile(download_abspath):
+            print('Downloading {} from {}'.format(download_filename, download_src))
+            iter_content = self.__try_to_get(download_src, is_imgur=is_imgur, stream=True)
+            if iter_content:
+                with open(download_abspath, 'wb') as download:
+                    for chunk in iter_content:
+                        download.write(chunk)
                 print('Saved')
         else:
-            print('{} has already been downloaded'.format(download_main_filename))
+            print('{} has already been downloaded'.format(download_filename))
         return bool(content)
     
-    def simple_download(self, year, line_number, download_dirname, download_fileroot, download_main_ext, download_main_src, is_imgur=False):
-        return self.__handle_download(year, line_number, self.__simple_download, download_dirname, download_fileroot, download_main_ext, download_main_src, is_imgur=is_imgur)
+    def simple_download(self, year, line_number, download_dirname, download_fileroot, download_ext, download_src, is_imgur=False):
+        return self.__handle_download(year, line_number, self.__simple_download, download_dirname, download_fileroot, download_ext, download_src, is_imgur=is_imgur)
 
     def __imgur_page_download(self, download_dirname, download_fileroot, media_infos):
         all_went_well = True
@@ -282,12 +305,12 @@ class FileManager(object):
     def __reddit_video_download(self, download_dirname, download_fileroot, dash_url_escaped):
         # TODO: Mix with storyboard if video is not available, like RapidSave does
         dash_url = html.unescape(dash_url_escaped)
-        download_main_filename = '{}.%(ext)s'.format(download_fileroot)
-        download_main_abspath = os.path.join(download_dirname, download_main_filename)
-        print('Downloading {} from {}'.format(download_main_filename, dash_url))
+        download_filename = '{}.%(ext)s'.format(download_fileroot)
+        download_abspath = os.path.join(download_dirname, download_filename)
+        print('Downloading {} from {}'.format(download_filename, dash_url))
         ydl_opts = {
             'logger': NoLog,
-            'outtmpl': download_main_abspath,
+            'outtmpl': download_abspath,
             'continuedl': False,
             'overwrites': True
         }
@@ -313,6 +336,22 @@ class FileManager(object):
 
     def reddit_video_download(self, year, line_number, download_dirname, download_fileroot, dash_url_escaped):
         return self.__handle_download(year, line_number, self.__reddit_video_download, download_dirname, download_fileroot, dash_url_escaped)
+
+class Estimator(object):
+    def __init__(self, current_total_size):
+        self.__current_total_size = current_total_size
+        self.__downloaded_post_count = 0
+        self.__total_post_count = 0
+    
+    def increment(self, is_downloaded):
+        if is_downloaded:
+            self.__downloaded_post_count += 1
+        self.__total_post_count += 1
+    
+    def estimate(self):
+        post_count_ratio = self.__downloaded_post_count / self.__total_post_count
+        estimated_total_size = self.__current_total_size / post_count_ratio
+        print('About {:,} more bytes needed, with {}% confidence\n(currently at {:,} of {:,} posts downloaded with {:,} bytes used)'.format(math.ceil(estimated_total_size * (1 - post_count_ratio)), math.ceil(post_count_ratio * 100000000) / 1000000, self.__downloaded_post_count, self.__total_post_count, self.__current_total_size))
 
 def main() -> int:
     cwd = getcwd()
@@ -361,9 +400,7 @@ def main() -> int:
             return 1
         else:
             print('Checking space used...')
-            current_post_count = 0
-            total_post_count = 0
-            current_total_size = folder_size(download_basedirname)
+            estimator = Estimator(folder_size(download_basedirname))
             print('Checking archive progress...')
 
         line_number = -1
@@ -371,6 +408,7 @@ def main() -> int:
             line_number += 1
             try:
                 line_is_bookmark = file_manager.is_bookmark(line_number)
+                line_could_be_a_downloaded_post = last_bookmark_not_reached and line_is_bookmark == IS_NOT_A_BOOKMARK
                 if last_bookmark_not_reached:
                     if download_mode and line_is_bookmark == IS_NOT_A_BOOKMARK:
                         continue
@@ -404,10 +442,7 @@ def main() -> int:
                                 single_image_match.group(1),
                                 is_imgur = single_image_match.group(2).endswith('imgur.com'))
                         else:
-                            # TODO: Make a post count tracker object for estimate mode
-                            if last_bookmark_not_reached and line_is_bookmark == IS_NOT_A_BOOKMARK:
-                                current_post_count += 1
-                            total_post_count += 1
+                            estimator.increment(line_could_be_a_downloaded_post)
                     else:
                         # Imgur page (media or album)
                         imgur_match = re.match(r'^https?:\/\/imgur\.com\/((a|gallery)\/)?([a-zA-Z0-9,]+)($|[^\/a-zA-Z0-9])', url)
@@ -430,9 +465,7 @@ def main() -> int:
                                     download_fileroot,
                                     media_infos)
                             else:
-                                if last_bookmark_not_reached and line_is_bookmark == IS_NOT_A_BOOKMARK:
-                                    current_post_count += 1
-                                total_post_count += 1
+                                estimator.increment(line_could_be_a_downloaded_post)
 
                 # Reddit gallery
                 gallery_items = chain_get(data, 'gallery_data', 'items')
@@ -446,9 +479,7 @@ def main() -> int:
                             download_fileroot,
                             gallery_items)
                     else:
-                        if last_bookmark_not_reached and line_is_bookmark == IS_NOT_A_BOOKMARK:
-                            current_post_count += 1
-                        total_post_count += 1
+                        estimator.increment(line_could_be_a_downloaded_post)
 
                 # Reddit video
                 dash_url = chain_get(data, 'media', 'reddit_video', 'dash_url')
@@ -461,9 +492,7 @@ def main() -> int:
                             download_fileroot,
                             dash_url)
                     else:
-                        if last_bookmark_not_reached and line_is_bookmark == IS_NOT_A_BOOKMARK:
-                            current_post_count += 1
-                        total_post_count += 1
+                        estimator.increment(line_could_be_a_downloaded_post)
             except Exception as e:
                 print(e)
                 file_manager.add_bookmark(line_number)
@@ -477,9 +506,7 @@ def main() -> int:
     elif not_interrupted:
         # In estimate mode and both the bookmark and the end of the archive
         # were reached, i.e. the estimate can and will be made
-        post_count_ratio = current_post_count / total_post_count
-        estimated_total_size = current_total_size / post_count_ratio
-        print('About {:,} more bytes needed, with {}% confidence\n(currently at {:,} of {:,} posts downloaded with {:,} bytes used)'.format(math.ceil(estimated_total_size * (1 - post_count_ratio)), math.ceil(post_count_ratio * 100000000) / 1000000, current_post_count, total_post_count, current_total_size))
+        estimator.estimate()
     else:
         # In estimate mode and the end of the archive was not reached
         print('Estimation cancelled!')
